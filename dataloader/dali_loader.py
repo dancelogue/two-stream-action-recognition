a72573a7203fc402ee990e3416137a9056e84b5f
Mark Gituma <gitumarkk@gmail.com>
2019-07-25 01:17:34 -0400

    updated dali loader

--- dataloader/dali_loader.py
+++ dataloader/dali_loader.py
@@ -9,8 +9,9 @@ import nvidia.dali.ops as ops
 import nvidia.dali.types as types
 from nvidia.dali.plugin import pytorch
 
+
 class VideoReaderPipeline(Pipeline):
-    def __init__(self, batch_size, sequence_length, num_threads, device_id, files, crop_size):
+    def __init__(self, batch_size, sequence_length, num_threads, device_id, files, crop_size, transform=None):
         super(VideoReaderPipeline, self).__init__(batch_size, num_threads, device_id, seed=12)
         self.reader = ops.VideoReader(
             device='gpu',
@@ -22,7 +23,38 @@ class VideoReaderPipeline(Pipeline):
             dtype=types.UINT8,
             initial_fill=16
         )
+        self.transform = transform
 
         def define_graph(self):
             output = self.input(name='Reader')
-            return outputv 
+
+            if self.transform:
+                output = self.transform(output)
+            return output
+
+
+class DaliLoader():
+    def __init__(self, batch_size, file_root, sequence_length, crop_size):
+        container_files = [file_root + '/' + f for f in os.listdir(file_root)]
+        self.pipeline = VideoReaderPipeline(
+            batch_size=batch_size,
+            sequence_length=sequence_length,
+            num_threads=2,
+            device_id=0,
+            files=container_files,
+            crop_size=crop_size
+        )
+        self.pipeline.build()
+        self.epoch_size = self.pipeline.epoch_size('Reader')
+        self.dali_iterator = pytorch.DALIGenericIterator(
+            self.pipeline,
+            ["data"],
+            self.epoch_size,
+            auto_reset=True
+        )
+
+    def __len__(self):
+        return int(self.epoch_size)
+
+    def __iter__(self):
+        return self.dali_iterator.__iter__()
